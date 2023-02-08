f936cfbf191f45f30b215aebe0e0b5df7a880ebc
Daniel Barba <frdb103@gmail.com>
2023-02-08 15:57:07 -0500

    UI syntax update

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,131 +1,145 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import click from "../assets/click.wav"
 
-function playSound(){
+function playSound() {
   new Audio(click).play()
 }
 
-export default function Calculator(){
-    const [input, setInput] = useState("")
-    const [oldInput, setOldInput] = useState("")
-    const [operator, setOperator] = useState("")
-    
-  
-    return(
+export default function Calculator() {
+  const [input, setInput] = useState("")
+  const [oldInput, setOldInput] = useState("")
+  const [operator, setOperator] = useState("")
+
+
+  return (
     <>
-<div className="flex-container">
+      <div className="flex-container">
 
-    <div className="calculator-body">
-      <div className="lcd">
-           <p className="lcd-numbers">{input}</p> 
+        <div className="calculator-body">
+          <div className="lcd">
+            <p className="lcd-numbers">{input}</p>
+          </div>
+    <br/>
+          <div className="button-container" onClick={playSound}>
+            <div>
+              <button onClick={() => {
+                AddToinput(setInput, input, 7)
+              }}>7</button>
+
+              <button onClick={() => {
+                AddToinput(setInput, input, 8)
+              }}>8 </button>
+
+              <button onClick={() => {
+                AddToinput(setInput, input, 9)
+              }}>9</button>
+
+              <button onClick={() => {
+                safeSetOperator(oldInput, setOldInput, input, setInput, operator, setOperator, "+")
+              }}>+</button>
+
+            </div>
+            <div>
+              <button onClick={() => {
+                AddToinput(setInput, input, 4)
+              }}>4</button>
+
+              <button onClick={() => {
+                AddToinput(setInput, input, 5)
+              }}>5 </button>
+
+              <button onClick={() => {
+                AddToinput(setInput, input, 6)
+              }}>6</button>
+              <button onClick={() => {
+                safeSetOperator(oldInput, setOldInput, input, setInput, operator, setOperator, "-")
+              }}>-</button>
+            </div>
+            <div>
+              <button onClick={() => {
+                AddToinput(setInput, input, 1)
+              }}>1</button>
+
+              <button onClick={() => {
+                AddToinput(setInput, input, 2)
+              }}>2 </button>
+
+              <button onClick={() => {
+                AddToinput(setInput, input, 3)
+              }}>3</button>
+
+
+              <button onClick={() => {
+                safeSetOperator(oldInput, setOldInput, input, setInput, operator, setOperator, "*")
+              }}>x</button>
+
+            </div>
+            <div>
+              <button onClick={() => {
+                AddToinput(setInput, input, 0)
+              }}>0</button>
+
+              <button onClick={() => {
+                if (!input) setInput("0.")
+                if (!input.includes(".")) AddToinput(setInput, input, ".")
+              }}>.</button>
+
+              <button onClick={() => {
+                safeSetOperator(oldInput, setOldInput, input, setInput, operator, setOperator, "/")
+              }}>/</button>
+
+              <button onClick={() => {
+                const calc = runCalculation(oldInput,setOldInput,input,setInput,operator,setOperator)
+                setInput(calc)
+                setOldInput(undefined)
+                setOperator(undefined)
+              }}>=</button>
+            </div>
+
+            <div >
+              <button onClick={() => {
+                setInput(undefined)
+                setOldInput(undefined)
+                setOperator(undefined)
+              }}>C</button>
+
+            </div>
+
+          </div>
+        </div>
       </div>
-
-   <div className="button-container" onClick={playSound}>
-        <div>
-          <button onClick={()=>{
-              AddToinput(setInput,input,7)
-            }}>7</button>
-
-          <button onClick={()=>{
-              AddToinput(setInput,input,8)
-            }}>8 </button>
-            
-          <button onClick={()=>{
-              AddToinput(setInput,input,9)
-            }}>9</button>
-
-            <button onClick={()=>{
-              safeSetOperator(setOldInput,input,setInput,operator,setOperator,"+")
-            }}>+</button>
-            
-        </div>
-        <div>
-          <button onClick={()=>{
-              AddToinput(setInput,input,4)
-            }}>4</button>
-
-          <button onClick={()=>{
-              AddToinput(setInput,input,5)
-            }}>5 </button>
-            
-          <button onClick={()=>{
-              AddToinput(setInput,input,6)
-            }}>6</button>
-             <button onClick={()=>{
-               safeSetOperator(setOldInput,input,setInput,operator,setOperator,"-")
-            }}>-</button>
-        </div>
-        <div>
-          <button onClick={()=>{
-              AddToinput(setInput,input,1)
-            }}>1</button>
-
-          <button onClick={()=>{
-              AddToinput(setInput,input,2)
-            }}>2 </button>
-            
-          <button onClick={()=>{
-              AddToinput(setInput,input,3)
-            }}>3</button>
-            
-
-            <button onClick={()=>{
-              safeSetOperator(setOldInput,input,setInput,operator,setOperator,"*")  
-            }}>x</button>
-
-        </div>
-        <div>
-        <button onClick={()=>{
-              AddToinput(setInput,input,0)
-            }}>0</button>
-
-            <button onClick={()=>{
-              if(!input)setInput("0.")
-              if(!input.includes("."))AddToinput(setInput,input,".")
-            }}>.</button>
-
-            <button onClick={()=>{
-            safeSetOperator(setOldInput,input,setInput,operator,setOperator,"/")
-            }}>/</button>
-            
-            <button onClick={()=>{
-              let calc = 0
-              if(operator==="+")calc = Number(input) +  Number(oldInput)
-              if(operator==="-")calc = Number(oldInput) -  Number(input)
-              if(operator==="*")calc = Number(input) * Number(oldInput)
-              if(operator==="/")calc = Number(oldInput) / Number(input)
-            
-              setOldInput(undefined)
-              setOperator(undefined)
-              setInput(calc)
-            }}>=</button>
-        </div>
-
-        <div > 
-        <button onClick={()=>{
-              setInput(undefined)
-              setOldInput(undefined)
-              setOperator(undefined)
-            }}>C</button>
-
-        </div>
-        
-    </div>
-  </div>
-</div>
-        </>
-    )}
-            
-function safeSetOperator(setOld, New, setNew, op, setOp, opType){
-  if(op==="") setOp(opType)
-  else if(op!=opType){
-    setOp(opType)
-    setOld(New)
-    setNew(undefined)
-    }
+    </>
+  )
 }
 
-function AddToinput( setPass, pass="", value=""){
+function runCalculation(Old, setOld, In, setIn, op, setOp) {
+  let calc = 0
+  if (op === "+") calc = Number(In) + Number(Old)
+  if (op === "-") calc = Number(Old) - Number(In)
+  if (op === "*") calc = Number(In) * Number(Old)
+  if (op === "/") calc = Number(Old) / Number(In)
 
-      if(pass.length < 15 ) setPass(pass.toString()+value.toString())
+  return calc
+}
+
+function safeSetOperator( Old, setOld, In, setIn, op, setOp, opType) {
+
+  if(Old && In && op){
+    const calc = runCalculation(Old, setOld, In, setIn, op, setOp)
+    setOp(opType)
+    setOld(calc)
+    setIn(undefined)
+    return
+  } 
+  if (op === "") setOp(opType)
+  else if (op != opType) {
+    setOp(opType)
+    setOld(In)
+    setIn(undefined)
+  }
+}
+
+function AddToinput( setPass, pass = "", value = "") {
+
+  if (pass.length < 15) setPass(pass.toString() + value.toString())
 }
